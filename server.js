0816ed031762855d4021cfb685c347023b73690e
Abdur Rahman Hijazi <AHijazi@users.noreply.github.com>
2017-04-30 11:53:01 +0400

    adding db

--- server.js
+++ server.js
@@ -4,34 +4,7 @@ possible on Facebook using the framework. The demo shows how to create a looping
 menu how send things like Pictures, Bubbles, Receipts, and use Carousels. It also
 shows all of the prompts supported by Bot Builder and how to receive uploaded
 photos, videos, and location.
-# RUN THE BOT:
-    You can run the bot locally using the Bot Framework Emulator but for the best
-    experience you should register a new bot on Facebook and bind it to the demo 
-    bot. You can run the bot locally using ngrok found at https://ngrok.com/.
-    * Install and run ngrok in a console window using "ngrok http 3978".
-    * Create a bot on https://dev.botframework.com and follow the steps to setup
-      a Facebook channel. The Facebook channel config page will walk you through 
-      creating a Facebook page & app for your bot.
-    * For the endpoint you setup on dev.botframework.com, copy the https link 
-      ngrok setup and set "<ngrok link>/api/messages" as your bots endpoint.
-    * Next you need to configure your bots MICROSOFT_APP_ID, and
-      MICROSOFT_APP_PASSWORD environment variables. If you're running VSCode you 
-      can add these variables to your the bots launch.json file. If you're not 
-      using VSCode you'll need to setup these variables in a console window.
-      - MICROSOFT_APP_ID: This is the App ID assigned when you created your bot.
-      - MICROSOFT_APP_PASSWORD: This was also assigned when you created your bot.
-    * Install the bots persistent menus following the instructions outlined in the
-      section below.
-    * To run the bot you can launch it from VSCode or run "node app.js" from a 
-      console window. 
-# INSTALL PERSISTENT MENUS
-    Facebook supports persistent menus which Bot Builder lets you bind to global 
-    actions. These menus must be installed using the page access token assigned 
-    when you setup your bot. You can easily install the menus included with the 
-    example by running the cURL command below:
-        curl -X POST -H "Content-Type: application/json" -d @persistent-menu.json 
-        "https://graph.facebook.com/v2.6/me/thread_settings?access_token=PAGE_ACCESS_TOKEN"
-    
+
 -----------------------------------------------------------------------------*/
 
 var restify = require('restify');
@@ -197,7 +170,7 @@ session.send('Hold on a sec.. I am searching for nearby stores');
 });
 
 bot.dialog('Help', function (session) {
-    session.endDialog('Hi! Try asking me things like \'Order potatos from ABC Store\', \'search a nearby store\' or \'order from City Taste Cafe\'');
+    session.endDialog('Hi! Try asking me things like \'Order potatos from ABC Store\', \'search a nearby store\' or \'order from City Taste Cafe\'' + 'Also i have got: ' + setupDBandask());
 }).triggerAction({
     matches: 'Help'
 });
@@ -247,4 +220,26 @@ function reviewAsAttachment(review) {
         .title(review.title)
         .text(review.text)
         .images([new builder.CardImage().url(review.image)]);
+}
+
+function setupDBandask(){
+
+    var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'us-cdbr-azure-southcentral-f.cloudapp.net',
+  user     : 'b84f40e80708f5',
+  password : '43bea69b',
+  database : 'illorderbot'
+});
+
+connection.connect();
+
+connection.query('SELECT item_name from items', function(err, rows, fields) {
+  if (!err)
+    return('The solution is: ', rows);
+  else
+    return('Error while performing Query.');
+});
+
+connection.end();
 }
